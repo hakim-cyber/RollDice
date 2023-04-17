9c372480faa97a694da5f94b5ad2294c648255b1
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-17 22:34:50 +0400

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by aplle on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
